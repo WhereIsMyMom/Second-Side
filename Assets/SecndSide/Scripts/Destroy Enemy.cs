97498cdc2bece4286acf7557e37f6fa933551141
Arvin Zheng <145510122+WhereIsMyMom@users.noreply.github.com>
2024-12-09 18:14:00 -0500

    hEALTH

--- Assets/SecndSide/Scripts/Destroy Enemy.cs
+++ Assets/SecndSide/Scripts/Destroy Enemy.cs
@@ -1,13 +1,19 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class DestroyEnemy : MonoBehaviour
 {
+
+    public int health = 100;
+    public int Damage = 20; 
+    public Image heart;
+
     // Start is called before the first frame update
     void Start()
     {
-
+        heart = GameObject.Find("Heart").GetComponent<Image>(); 
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
@@ -18,7 +24,8 @@ public class DestroyEnemy : MonoBehaviour
             // Coin tag, dicates what happens after player touches coins 
             case "Destroy":
                 {
-
+                    health -= Damage;
+                    heart.fillAmount = (float) health / (float) 100;
                     // Destroys the coins
                     Destroy(gameObject);
                     break;
